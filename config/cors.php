bf3bbc1544764ac499223f814d21f2d41612c3b3
Olaf <olaf.pohlmann@gmail.com>
2023-09-07 08:36:50 +0200

    CORS Configuration

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['login', 'logout', 'xrefs', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
